378953122feb9e8515e5adbeacab9389fce80648
vishelpodishat <alishsaideshov@gmail.com>
2023-10-02 14:36:48 +0600

    describing

---
+++ OrtaApp/Resources/AppColor.swift
@@ -0,0 +1,27 @@
+//
+//  AppColor.swift
+//  OrtaApp
+//
+//  Created by Алишер Сайдешев on 02.10.2023.
+//
+
+import UIKit
+
+protocol AppColorProtocol {
+    var rawValue: String { get }
+}
+
+extension AppColorProtocol {
+
+    var uiColor: UIColor {
+        UIColor.init(named: rawValue) ?? .white
+    }
+
+    var cgColor: CGColor {
+        return uiColor.cgColor
+    }
+}
+
+enum AppColor: String, AppColorProtocol {
+    case blue
+}
